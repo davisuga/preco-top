1256f2f8cff17a6407060de48f484b67c2b030d6
Davi Suga <daviciencia1@gmail.com>
2021-02-08 17:35:39 -0300

    rebuild test

--- pages/sas.js
+++ pages/sas.js
@@ -2,8 +2,13 @@ import React from "react";
 
 // import { Container } from './styles';
 
-function Sas() {
-  return <div>{JSON.stringify(process.env)}</div>;
+function Sas({ data }) {
+  return <div>{JSON.stringify(data)}</div>;
+}
+
+/* Retrieves pet(s) data from mongodb database */
+export async function getServerSideProps() {
+  return { props: { data: process.env } };
 }
 
 export default Sas;
